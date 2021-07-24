1f61bef49df466b7fb3ed09f9075e06a5361131e
gunjan <joshigunjan010@gmail.com>
2021-07-24 12:03:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(){
+    
+    var x = document.getElementsByClassName("bod")[0].children;
+
+    var y=[];
+       
+      
+        y= document.getElementsByClassName("txt")[0].appendChild(x[0]);
+    
+}
+
+
+function remove(){
+    var obj=document.getElementsByClassName("rytbox")[0].children;
+    
+    obj.removechild(obj.childnodes[0]);
+}
